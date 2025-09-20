ae825f87f005bd39ef7de595e138668b8116fca8
Schrieffer <xjack875@gmail.com>
2025-09-20 20:36:23 +0800

    update

--- 15.threeSum.cpp
+++ 15.threeSum.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 
 class Solution 
 {
@@ -7,6 +8,9 @@ public:
     std::vector<std::vector<int>> threeSum(std::vector<int>& nums) 
     {
         sort(nums.begin(), nums.end());
+
+        std::vector<std::vector<int>> res;
+
         for (int i = 0; i < nums.size(); i++)
         {
             int left = i+1, right = nums.size()-1;
@@ -14,7 +18,8 @@ public:
 
             while (left < right)
             {
-                
+                int sum = nums[left] + nums[right];
+                if (sum == target) res.push_back({nums[left], nums[right], target});
             }
         }
     }
