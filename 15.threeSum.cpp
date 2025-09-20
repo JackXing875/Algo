6ca59b0f81caedbded439ab36a4a641e75f4e150
Schrieffer <xjack875@gmail.com>
2025-09-20 20:47:49 +0800

    update

--- 15.threeSum.cpp
+++ 15.threeSum.cpp
@@ -13,14 +13,36 @@ public:
 
         for (int i = 0; i < nums.size(); i++)
         {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            
             int left = i+1, right = nums.size()-1;
             int target = -nums[i];
 
             while (left < right)
             {
                 int sum = nums[left] + nums[right];
-                if (sum == target) res.push_back({nums[left], nums[right], target});
+
+                if (sum == target)
+                {
+                    res.push_back({nums[left], nums[right], nums[i]});
+                    while (left < right && nums[left] == nums[left+1]) left++;
+                    while (left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                }
+                else if (sum < target)
+                {
+                    while (left < right && nums[left] == nums[left+1]) left++;
+                    left++;
+                }
+                else
+                {
+                    while (left < right && nums[right] == nums[right-1]) right--;
+                    right--;
+                }
             }
         }
+
+        return res;
     }
 };
