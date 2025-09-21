dc0641f979a2c253e5b2f18b70b8f80c63f31f41
Schrieffer <xjack875@gmail.com>
2025-09-21 19:42:42 +0800

    update

--- temp.cpp
+++ temp.cpp
@@ -8,5 +8,9 @@ bool com(T a, T b)
 
 int main(int argc, char *argv[])
 {
-
+    int a = 3, b = 5;
+    int x = 1, y = 1;
+    std::cout << com(a, b) << std::endl;
+    std::cout << com(x, y) << std::endl;
+    return 0;
 }
