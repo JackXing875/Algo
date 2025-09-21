3f4a6c237ff632c99f966bbb52280bac9dee2e97
Schrieffer <xjack875@gmail.com>
2025-09-21 19:49:11 +0800

    update

--- temp.cpp
+++ temp.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 template <typename T>
-bool com(T a, T b)
+bool compare(T a, T b)
 {
-    return ~(a ^ b);
+    return !(a ^ b);
 }
 
 int main(int argc, char *argv[])
 {
     int a = 3, b = 5;
     int x = 1, y = 1;
-    std::cout << com(a, b) << std::endl;
-    std::cout << com(x, y) << std::endl;
+    std::cout << compare(a, b) << std::endl;
+    std::cout << compare(x, y) << std::endl;
     return 0;
 }
