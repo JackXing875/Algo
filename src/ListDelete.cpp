b19735814f274a89e0b3aea00347545ecc36488e
Schrieffer <xjack875@gmail.com>
2025-10-12 20:45:33 +0800

    update

--- src/ListDelete.cpp
+++ src/ListDelete.cpp
@@ -11,55 +11,23 @@ public:
 };
 
 template <typename T>
-void deleteNode(ListNode<T> *&head)
+void deleteMin(ListNode<T> *head) 
 {
-    if (!head) return;
+    if (!head || !head->next) return; 
 
-    if (!head->next)
+    ListNode<T> *minPrev = head;     
+    ListNode<T> *curPrev = head;
+    ListNode<T> *cur = head->next;
+
+    while (cur) 
     {
-        delete head;
-        head = nullptr;
-        return;
-    }
-
-    ListNode<T> *minPrev = nullptr;
-    ListNode<T> *minNode = head;
-
-    ListNode<T> *prev = nullptr;
-    ListNode<T> *cur = head;
-
-    while (cur)
-    {
-        if (cur->data < minNode->data)
-        {
-            minNode = cur;
-            minPrev = prev;
-        }
-        prev = cur;
+        if (cur->data < minPrev->next->data)
+            minPrev = curPrev;  
+        curPrev = cur;
         cur = cur->next;
     }
 
-    if (minPrev)
-        minPrev->next = minNode->next;
-    else
-        head = minNode->next;
-
+    ListNode<T> *minNode = minPrev->next;
+    minPrev->next = minNode->next;
     delete minNode;
-}
-
-int main(int argc, char *argv[])
-{
-    ListNode<int>* head = new ListNode<int>(4);
-    head->next = new ListNode<int>(2);
-    head->next->next = new ListNode<int>(5);
-    head->next->next->next = new ListNode<int>(3);
-
-    deleteNode(head);
-
-    for (ListNode<int>* p = head; p; p = p->next)
-        std::cout << p->data << " ";
-    
-    std::cout << std::endl;
-
-    return 0;
 }
