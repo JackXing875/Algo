449f2236937eb02f56d36474830ec11d48b5abb0
Schrieffer <xjack875@gmail.com>
2025-11-07 15:42:07 +0800

    add 9 148

---
+++ src/148.sortList.cpp
@@ -0,0 +1,58 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution {
+public:
+    ListNode *findMid(ListNode *head) {
+        if (!head) return nullptr;
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *mid = slow->next;
+        slow->next = nullptr;
+
+        return mid;
+    }
+
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* cur = &dummy;
+
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                cur->next = list1;
+                list1 = list1->next;
+            } else {
+                cur->next = list2;
+                list2 = list2->next;
+            }
+            cur = cur->next;
+        }
+
+        cur->next = list1 ? list1 : list2;
+        return dummy.next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode *mid = findMid(head);
+
+        ListNode *left = sortList(head);
+        ListNode *right = sortList(mid);
+
+        return merge(left, right);
+    }
+};
